d1a1b3097b59f009439d387bfd62a24b9e391cf4
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2025-07-28 12:27:41 +0100

    Updated Meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sasere Portfolio",
   description: `Code Alchemist | Frontend Engineer & Mobile Developer
-Turning ideas into fast, scalable apps with React Native, TypeScript, Supabase, and FastAPI. Focused on clean code, great UX, and real-world impact.`,
+Turning ideas into fast, scalable apps with React Native, TypeScript. Focused on clean code, great UX, and real-world impact.`,
   keywords: ['Frontend Engineer', 'React Native Developer', 'TypeScript', 'Supabase', 'Mobile App Developer', 'Fullstack Developer', 'FastAPI', 'Software Engineer', 'Scalable Apps', 'Clean Code', 'UI/UX', 'Tech Innovator']
 };
 
